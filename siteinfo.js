3edb304d9825face29e18cd65796462f5b2e319a
[wangguang1995] <[652990306@qq.com]>
2018-04-12 13:10:29 +0800

    jiajian1.0

---
+++ siteinfo.js
@@ -0,0 +1,11 @@
+var siteinfo = {
+  "name": 'we7_wxappdemo',
+  "uniacid": "944",
+  "acid": "944",
+  "multiid": "0",
+  "version": "1.0.0",
+  "siteroot": "http://www.fan.cc/app/index.php",
+  'method_design': '3'
+};
+module.exports = siteinfo;    
+
---
+++ we7/pages/home/home.js
@@ -0,0 +1,112 @@
+// pages/home/home.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tabArr: {
+      curHdIndex: 0,
+      curBdIndex: 0,
+      isTrue:null
+    }, 
+    num:2332,
+    wnum:20
+  },
+  display:function(e){
+    this.setData({
+      isTrue:true
+    })
+
+  },
+  displayHide:function(e){
+    this.setData({
+      isTrue:false
+    })
+  },
+  listSwitching:function(e){
+    var dataId = e.currentTarget.id;
+    var obj = {};
+    obj.curHdIndex = dataId;
+    obj.curBdIndex = dataId;
+    this.setData({
+      tabArr: obj
+    })
+  },
+  answer:function(e){
+    wx.redirectTo({
+      url:'../transfer/transfer'
+    })
+  },
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '自定义转发标题',
+      path: '/page/user?id=123',
+      success: function (res) {
+        // 转发成功
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
